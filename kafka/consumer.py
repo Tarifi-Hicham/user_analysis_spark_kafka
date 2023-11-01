abaa3cbc3d50ed2a22c8a0422c7d460658e3f406
Tarifi Hicham <tarifi.hicham@gmail.com>
2023-11-01 15:23:55 +0100

    add comments to kafka consumer

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -1,12 +1,16 @@
 from pykafka import KafkaClient
 import json
 
-topic_name = "randomuser_data"
-kafka_hosts = "localhost:9092"  # Change this to your Kafka server address
+# Kafka server address
+kafka_hosts = "localhost:9092"  
+# topic name
+topic_name = "hicham_topic"
 
-# Create a Kafka client
+# Kafka client
 client = KafkaClient(hosts=kafka_hosts)
+# specifie the topic
 topic = client.topics[topic_name]
+# specifie the consumer
 consumer = topic.get_simple_consumer()
 
 # Consume messages from the Kafka topic
@@ -14,7 +18,7 @@ for message in consumer:
     if message is not None:
         data = json.loads(message.value.decode('utf-8'))
         # Process the data as needed
-        print(data)  # Example: Print the data to the console
+        print(data)
 
 # Close the consumer connection (optional)
 consumer.stop()
